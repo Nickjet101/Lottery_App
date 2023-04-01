033c60d905cf353556cd8fa0d6f717ec3c88ba10
NICHOLAS EKENEDILICHUKWU ENEH <127051297+Nickjet101@users.noreply.github.com>
2023-04-01 09:58:59 +0100

    Update python_game.py

--- python_game.py
+++ python_game.py
@@ -1,18 +1,21 @@
+
 import random
 
 
+winning_number = 30
+username = input("Enter your name to play: ")
+print("Hello", username, "welcome to cohort 1 lucky lottery")
 
+def lottery_app():
+    while 1:
+        usernumber = int(input("enter any number from 0 to 50: "))
+        random_number = random.randint(0, 50)
+        if usernumber > 50:
+            print("Your input number is more than 50, sorry you are disqualified, try again later.")
+            exit() 
+        elif usernumber == winning_number == random_number:
+            print("congratulations!", username, "you won")
+        else:
+            print("sorry", username, "you lost, you can try again")
 
-def Lucky_Winner():
-      while 1:
-             Username = input("Enter your Name, ")
-             Users_Number = int(input("Enter your Lottery Number 0 to 50, "))
-             Numbers = random.randint(0, 50)
-             if Numbers == 30:
-                    print("congratulations", Username, "You won")
-             else:
-                    print("Sorry!", Username, "you lost, please try again.")
-
-
-
-Lucky_Winner()
+lottery_app()
